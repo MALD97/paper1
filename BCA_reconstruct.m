b287cd1e90a7a1e2dab2f3de5a466219595f8c02
MALD97 <107589999+MALD97@users.noreply.github.com>
2022-08-04 08:20:23 +0800

    Update BCA_reconstruct.m

--- BCA_reconstruct.m
+++ BCA_reconstruct.m
@@ -14,7 +14,7 @@ E=randn(m,q)+1i*randn(m,q);
 B= E./repmat(sqrt(sum(abs(W'*E).^2)),m,1); %the matrix consists of one-dimension projection vector
 L=perimeter(B,z); %obtain the vector of convex perimeter after projection
 G=(1/sqrt(2))*(randn(q,n)+1i*randn(q,n)); %initialization of H after projection
-P=eye(270)-B'*inv(B*B')*B; %the matrix in the constrain condition
+R=eye(270)-B'*inv(B*B')*B; %the matrix in the constrain condition
 [Cost(1),penalty(1)]=EvalCost_penalty(G,L,P,alpha);
 i=1;
 Continue=1;%one of the flags of the loop
@@ -23,11 +23,11 @@ i=i+1;
 for j=1:q
 GradG(j,:)=-8*(L(j,1)-4*norm(G(j,:),1))*[(G(j,1)^(1/2)*(G(j,1)')^(-1/2))/2 (G(j,2)^(1/2)*(G(j,2)')^(-1/2))/2 (G(j,3)^(1/2)*(G(j,3)')^(-1/2))/2];
 end
-GradG_p=alpha*P'*P*G; %part of gradient
+GradG_p=alpha*R'*R*G; %part of gradient
 NGrad=GradG+GradG_p; % gradient of whole function
 mu=0.7*Cost(i-1)/(trace(NGrad'*NGrad));% step-size
 G=G-mu*NGrad;
-[Cost(i),penalty(i)]=EvalCost_penalty(G,L,P,alpha);
+[Cost(i),penalty(i)]=EvalCost_penalty(G,L,R,alpha);
 %Check whrether the function reach its convergence
 if i>checkpoint
     Dist=20;
@@ -63,8 +63,8 @@ for i=1:q
 end
 end
 %%--------------------------------------------------------------------
-function [Cost,penalty]=EvalCost_penalty(G,L,P,alpha)
+function [Cost,penalty]=EvalCost_penalty(G,L,R,alpha)
 e=L-4*sum(abs(G),2); %4==>for BPSK
-penalty=norm(P*G)^2;
+penalty=norm(R*G)^2;
 Cost=norm(e)^2+alpha*penalty;
 end
